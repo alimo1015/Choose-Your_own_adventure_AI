511f17f5382087bbab5d57c56c0278e106a14043
Ali Mohamed <alim1015@uw.edu>
2025-11-04 17:43:04 -0800

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import StoryLoader from './components/StoryLoader'
+import StoryGenerator from './components/StoryGenerator'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <Router>
+      <div className="app-container">
+        <header>
+          <h1>Interactive Story Generator</h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<StoryGenerator/>}/>
+            <Route path="/story/:id" element={<StoryLoader/>}/>
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  )
+}
+
+export default App
